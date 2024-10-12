9a8701d771468a6c8615084e3d541f6bd693a686
Anismelow <117463292+Anismelow@users.noreply.github.com>
2024-10-12 18:00:05 +0200

    se agrego la funcion de poder mandar correos  y variables de entorno

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
-
+from .form import EmailForm
 
 # Create your views here.
 
+
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    Paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+            posts  = Paginator.page(page_number)
+    except PageNotAnInteger:
+            posts = Paginator.page(1) 
+
+    except EmptyPage:
+            posts = Paginator.page(Paginator.num_pages)
+                
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
@@ -17,9 +30,36 @@ def post_detail(request, year, month, day, post):
         publish__year=year,
         publish__month=month,
         publish__day=day,
-        slug=post,  # se utiliza el slug para encontrar el post correctamente en la base de datos.  # Este es un ejemplo de cómo se puede utilizar el slug en las urls.py. En realidad, debes usar un campo que sea único y único para cada post.  # Este es un ejemplo de cómo se puede utilizar el slug en las urls.py
+        slug=post,  
         )
     return render(request, 'blog/post/detail.html', {'post': post})
 
 
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
